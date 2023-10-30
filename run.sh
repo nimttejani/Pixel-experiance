8ba5ef6e387f85934e4fa29f03238c0b6bd32cc1
Neet Tejani <134366616+nimttejani@users.noreply.github.com>
2023-10-30 13:25:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,17 @@ sudo apt install make build-essential libncurses-dev bison flex libssl-dev libel
 sleep 2
 git clone https://github.com/akhilnarang/scripts && cd scripts
 sleep 2
-bash /setup/android_build_env.sh
+./setup/android_build_env.sh
 sleep 2
 mkdir android/
 mkdir android/pe/
 sleep 2 
-curl https://storage.googleapis.com/git-repo-downloads/repo > sudo /usr/bin/repo
+wget https://storage.googleapis.com/git-repo-downloads/repo
+sudo mv /usr/bin/repo
 sudo chmod a+x /usr/bin/repo
 cd /android/pe
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
