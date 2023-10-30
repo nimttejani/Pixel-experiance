a8df154a3b785ec3c6626a083128a7e917208d2b
Neet Tejani <134366616+nimttejani@users.noreply.github.com>
2023-10-30 11:56:41 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git clone https://github.com/akhilnarang/scripts && cd scripts
 bash /setup/android_build_env.sh
 mkdir bin/
 mkdir android/
-mkdir /android/pe/
+mkdir android/pe/
 curl https://storage.googleapis.com/git-repo-downloads/repo > sudo /usr/local/bin/repo
 sudo chmod a+x /usr/local/bin/repo
 cd /android/pe
