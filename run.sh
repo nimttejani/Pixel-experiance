8044803bed793ae55e38f6d53a2b1d520efe5c55
Neet Tejani <134366616+nimttejani@users.noreply.github.com>
2023-10-30 12:00:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-sudo apt install make build-essential libncurses-dev bison flex libssl-dev libelf-dev cmake clang cpp gcc libncurses5 android-sdk-platform-tools git -y
-cd ~/
+sudo apt install make build-essential libncurses-dev bison flex libssl-dev libelf-dev cmake clang cpp gcc libncurses5 android-sdk-platform-tools git python3 python3-pip -y
+sleep 2
 git clone https://github.com/akhilnarang/scripts && cd scripts
+sleep 2
 bash /setup/android_build_env.sh
-mkdir bin/
+sleep 2
 mkdir android/
 mkdir android/pe/
-curl https://storage.googleapis.com/git-repo-downloads/repo > sudo /usr/local/bin/repo
-sudo chmod a+x /usr/local/bin/repo
+sleep 2 
+curl https://storage.googleapis.com/git-repo-downloads/repo > sudo /usr/bin/repo
+sudo chmod a+x /usr/bin/repo
 cd /android/pe
 repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
